891c672ec2ef29504b4cb0fc90f34431b59fe45f
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-09-10 14:25:50 -0700

    movement dropdown working

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,9 +1,31 @@
 <template>
-    <h2>User Dashboard</h2>
+    <div>
+        <h2>User Dashboard</h2>
+        <WorkoutCard :movements="movements"/>
+    </div>
 </template>
 
 <script>
+import WorkoutCard from './WorkoutCard.vue';
+import { getMovements } from '../services/api';
 export default {
+  data(){
+    return {
+      movements: null
+    };
+  },
+  created() {
+    getMovements()
+      .then(movements => {
+        this.movements = movements;
+      })
+      .catch(err => {
+        this.error = err;
+      });
+  },
+  components: {
+    WorkoutCard
+  }
 
 };
 </script>
---
+++ src/components/WorkoutCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Excercise 1</p>
+        <p>Exercise 2</p>
+        <form>
+            <select>
+                <option v-for="movement in movements"
+                :movement="movement"
+                :key="movement.id"
+                :name="movement.name">{{ movement.name }}</option>
+            </select>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movements: Array
+  }
+
+};
+</script>
+
+<style>
+
+</style>
