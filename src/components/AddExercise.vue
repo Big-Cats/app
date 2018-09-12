ae536eb8532d599eb87a3cb7a324f334965a75ba
Jack <ajackofsometrades@gmail.com>
2018-09-12 13:46:07 -0700

    adding reps and sets to add exercise form

--- src/components/AddExercise.vue
+++ src/components/AddExercise.vue
@@ -1,28 +1,58 @@
 <template>
   <form>
     <FormControl label="Add Exercise">
-        <select 
-          v-if="muscleMovements"
-          v-model="selectedMuscle"
-          v-on:change="handleChange"
-        >
-          <option 
-            v-for="muscle in muscles"
-            :key="muscle.id"
+
+        <FormControl label="Muscles">
+          <select 
+            v-if="muscleMovements"
+            v-model="selectedMuscle"
+            v-on:change="handleChange"
           >
-            {{ muscle.name }}
-          </option>
-        </select>
-        <select>
-          <option 
-            v-for="movement in muscleMovements[this.selectedMuscle]"
-            :movement="movement"
-            :key="movement.id"
-            :name="movement.name"
-          >
-            {{ movement.name }}
-          </option>
-        </select>
+            <option 
+              v-for="muscle in muscles"
+              :key="muscle.id"
+            >
+              {{ muscle.name }}
+            </option>
+          </select>
+        </FormControl>
+        
+        <FormControl label="Movements">
+          <select>
+            <option 
+              v-for="movement in muscleMovements[this.selectedMuscle]"
+              :movement="movement"
+              :key="movement.id"
+              :name="movement.name"
+            >
+              {{ movement.name }}
+            </option>
+          </select>
+        </FormControl>
+
+        <FormControl label="Sets">
+          <select>
+            <option
+              v-for="element in [1,2,3,4,5,6,7,8,9,10]"
+              :key="element"
+            >
+              {{ element }}
+            </option>
+          </select>          
+        </FormControl>
+
+        <FormControl label="Reps">
+          <select>
+            <option
+              v-for="element in [1,2,3,4,5,6,7,8,9,10]"
+              :key="element"
+            >
+              {{ element }}
+            </option>
+          </select>          
+        </FormControl>
+
+
       </FormControl>
       <button @click.prevent="onExerciseAdd">Add</button>
   </form>
